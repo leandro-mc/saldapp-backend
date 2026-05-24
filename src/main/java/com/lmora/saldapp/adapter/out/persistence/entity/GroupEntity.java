4099e3e1a379b0d642658c987c583c0afd4eca1d
Leandro <144516539+Leandro-MC@users.noreply.github.com>
2026-05-24 17:50:27 -0600

    feat(group): add GroupEntity and GroupJpaRepository for group persistence

---
+++ src/main/java/com/lmora/saldapp/adapter/out/persistence/entity/GroupEntity.java
@@ -0,0 +1,37 @@
+package com.lmora.saldapp.adapter.out.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "groups")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "group_name", nullable = false, length = 200)
+    private String name;
+
+    @Length(max = 500)
+    private String description;
+
+    @Column(name = "start_date",  nullable = false)
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
---
+++ src/main/java/com/lmora/saldapp/adapter/out/persistence/repository/GroupJpaRepository.java
@@ -0,0 +1,8 @@
+package com.lmora.saldapp.adapter.out.persistence.repository;
+
+import com.lmora.saldapp.adapter.out.persistence.entity.GroupEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface GroupJpaRepository extends JpaRepository<GroupEntity,Long> {
+
+}
