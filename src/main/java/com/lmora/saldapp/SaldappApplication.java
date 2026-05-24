8e224a64396387cae93dbd258c3ba442bccc7d98
Leandro <144516539+Leandro-MC@users.noreply.github.com>
2026-05-24 14:50:57 -0600

    chore: initialize spring boot project, configure postgresql connection and docker-compose from db

---
+++ src/main/java/com/lmora/saldapp/SaldappApplication.java
@@ -0,0 +1,13 @@
+package com.lmora.saldapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaldappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaldappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lmora/saldapp/SaldappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmora.saldapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaldappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
