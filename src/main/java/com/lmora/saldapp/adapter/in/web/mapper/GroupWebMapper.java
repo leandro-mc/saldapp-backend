3f55466ac9b7d1f640a341be5f8364191765f324
Leandro <144516539+Leandro-MC@users.noreply.github.com>
2026-05-26 00:23:27 -0600

    feat(mapper): update GroupWebMapper and IntegrantWebMapper to ignore specific fields during mapping for warnings

--- src/main/java/com/lmora/saldapp/adapter/in/web/mapper/GroupWebMapper.java
+++ src/main/java/com/lmora/saldapp/adapter/in/web/mapper/GroupWebMapper.java
@@ -5,10 +5,19 @@ import com.lmora.saldapp.adapter.in.web.dto.group.GroupResponse;
 import com.lmora.saldapp.adapter.in.web.dto.group.UpdateGroupRequest;
 import com.lmora.saldapp.domain.model.Group;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface GroupWebMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "startDate", ignore = true)
+    @Mapping(target = "endDate", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
     Group toDomain(UpdateGroupRequest request);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "endDate", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
     Group toDomain(CreateGroupRequest request);
 
     GroupResponse toDto(Group group);
--- src/main/java/com/lmora/saldapp/adapter/in/web/mapper/IntegrantWebMapper.java
+++ src/main/java/com/lmora/saldapp/adapter/in/web/mapper/IntegrantWebMapper.java
@@ -4,10 +4,14 @@ import com.lmora.saldapp.adapter.in.web.dto.integrant.IntegrantRequest;
 import com.lmora.saldapp.adapter.in.web.dto.integrant.IntegrantResponse;
 import com.lmora.saldapp.domain.model.Integrant;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface IntegrantWebMapper {
-
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "groupId", ignore = true)
     Integrant toDomain(IntegrantRequest request);
+
     IntegrantResponse toDto(Integrant integrant);
 }
