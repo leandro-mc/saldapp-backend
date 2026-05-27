907691d634f40b67714086d069ad3d78727d315f
Leandro <144516539+Leandro-MC@users.noreply.github.com>
2026-05-27 02:41:21 -0600

    chore(integrant): refactor for extract group and integrant lookup into private helpers and add throw exception when deleting integrant with associated expenses

--- src/main/java/com/lmora/saldapp/application/usecase/IntegrantUseCaseImpl.java
+++ src/main/java/com/lmora/saldapp/application/usecase/IntegrantUseCaseImpl.java
@@ -1,9 +1,11 @@
 package com.lmora.saldapp.application.usecase;
 
 import com.lmora.saldapp.application.port.in.IntegrantUseCase;
+import com.lmora.saldapp.application.port.out.ExpenseRepositoryPort;
 import com.lmora.saldapp.application.port.out.GroupRepositoryPort;
 import com.lmora.saldapp.application.port.out.IntegrantRepositoryPort;
 import com.lmora.saldapp.domain.exception.IntegrantDontBelongToGroupException;
+import com.lmora.saldapp.domain.exception.IntegrantHasExpensesException;
 import com.lmora.saldapp.domain.exception.ResourceNotFoundException;
 import com.lmora.saldapp.domain.model.Group;
 import com.lmora.saldapp.domain.model.Integrant;
@@ -21,29 +23,23 @@ public class IntegrantUseCaseImpl implements IntegrantUseCase {
 
     private final IntegrantRepositoryPort integrantRepository;
     private final GroupRepositoryPort groupRepository;
+    private final ExpenseRepositoryPort expenseRepository;
 
     @Transactional
     @Override
     public Integrant createAndAssign(Long groupId, Integrant integrant) {
-        Group existingGroup = groupRepository.findById(groupId)
-                .orElseThrow(() -> new ResourceNotFoundException("Group", groupId));
+        Group existingGroup = findOpenGroup(groupId);
 
-        existingGroup.validateIsOpen();
-
-        integrant.setGroupId(groupId);
+        integrant.setGroupId(existingGroup.getId());
         return integrantRepository.save(integrant);
     }
 
     @Transactional
     @Override
     public Integrant update(Long groupId, Long integrantId, Integrant integrant) {
-        Group existingGroup = groupRepository.findById(groupId)
-                .orElseThrow(() -> new ResourceNotFoundException("Group", groupId));
+        Group existingGroup = findOpenGroup(groupId);
 
-        existingGroup.validateIsOpen();
-
-        Integrant existingIntegrant = integrantRepository.findById(integrantId)
-                .orElseThrow(() -> new ResourceNotFoundException("Integrant", integrantId));
+        Integrant existingIntegrant = findAndValidateIntegrant(existingGroup.getId(), integrantId);
 
         // SetChangedValues
         existingIntegrant.setName(integrant.getName());
@@ -54,20 +50,13 @@ public class IntegrantUseCaseImpl implements IntegrantUseCase {
     @Transactional
     @Override
     public void delete(Long groupId, Long integrantId) {
-        Group existingGroup = groupRepository.findById(groupId)
-                .orElseThrow(() -> new ResourceNotFoundException("Group", groupId));
+        Group existingGroup = findOpenGroup(groupId);
 
-        existingGroup.validateIsOpen();
+        Integrant existingIntegrant = findAndValidateIntegrant(existingGroup.getId(), integrantId);
 
-        Integrant existingIntegrant = integrantRepository.findById(integrantId)
-                .orElseThrow(() -> new ResourceNotFoundException("Integrant", integrantId));
-
-        // Verify that the integrant belongs to the group before deleting
-        if (!Objects.equals(existingIntegrant.getGroupId(), groupId)) {
-            throw new IntegrantDontBelongToGroupException(integrantId, groupId);
+        if(!expenseRepository.findAllByIntegrant(existingIntegrant.getId()).isEmpty()) {
+            throw new IntegrantHasExpensesException(existingIntegrant.getId());
         }
-
-        // TODO: Verify that the integrant does not have associated expenses before deleting
 
         integrantRepository.deleteById(integrantId);
     }
@@ -75,6 +64,26 @@ public class IntegrantUseCaseImpl implements IntegrantUseCase {
     @Override
     public List<Integrant> findAllByGroup(Long groupId) {
         return integrantRepository.findAllByGroup(groupId);
+    }
+
+    private Group findOpenGroup(Long groupId) {
+        // Verify that the group exists
+        Group existingGroup = groupRepository.findById(groupId)
+                .orElseThrow(() -> new ResourceNotFoundException("Group", groupId));
+        // Verify that the group is not closed
+        existingGroup.validateIsOpen();
+        return existingGroup;
+    }
+
+    private Integrant findAndValidateIntegrant(Long groupId, Long integrantId) {
+        Integrant existingIntegrant = integrantRepository.findById(integrantId)
+                .orElseThrow(() -> new ResourceNotFoundException("Integrant", integrantId));
+
+        // Verify that the integrant belongs to the group
+        if (!Objects.equals(existingIntegrant.getGroupId(), groupId)) {
+            throw new IntegrantDontBelongToGroupException(existingIntegrant.getId(), groupId);
+        }
+        return existingIntegrant;
     }
 
 }
